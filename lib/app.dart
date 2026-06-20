b9e5f0c554cdd8742d55a0eb5102fadb62be990b
mazbaul20 <mazbaul20@gmail.com>
2026-06-20 17:48:14 +0600

    Navigator pushAndRemoveUntil

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
+import 'screen/login_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (_)=>HomeScreen()),
+                  (route) => false,
+                );
+              },
+              child: Text("Home Screen"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
